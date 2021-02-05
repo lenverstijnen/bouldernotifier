1370554c5dd70aca4dbb1ac12349dc7e47da5b7f
Len Verstijnen <lenverstijnen@me.com>
2021-02-05 15:02:25 +0100

    Add extractAvailableSlots

---
+++ src/extractAvailableSlots.test.ts
@@ -0,0 +1,30 @@
+import { parseISO } from "date-fns"
+import { extractAvailableSlots } from "./extractAvailableSlots"
+import { mockData } from "./mocks/mockData"
+
+it("Should return slot 173631 and 173634 if dateTime is 2021-02-02T07:00:00.000+01:00", () => {
+  const date = parseISO("2021-02-02T07:00:00.000+01:00")
+  const result = extractAvailableSlots(mockData, date)
+
+  expect(result?.length).toBe(2)
+  expect(result![0].id).toBe(173631)
+  expect(result![1].id).toBe(173634)
+})
+
+it("Should return slot 174043 and 174044 and 174046 and 174048 if dateTime is 2021-02-02T18:15:00.000+01:00", () => {
+  const date = parseISO("2021-02-02T18:15:00.000+01:00")
+  const result = extractAvailableSlots(mockData, date)
+
+  expect(result?.length).toBe(4)
+  expect(result![0].id).toBe(174043)
+  expect(result![1].id).toBe(174044)
+  expect(result![2].id).toBe(174046)
+  expect(result![3].id).toBe(174048)
+})
+
+it("Should return null if dateTime is 2021-02-02T16:45:00.000+01:00", () => {
+  const date = parseISO("2021-02-02T16:45:00.000+01:00")
+  const result = extractAvailableSlots(mockData, date)
+
+  expect(result).toBeNull()
+})
---
+++ src/extractAvailableSlots.ts
@@ -0,0 +1,61 @@
+import { differenceInMinutes, parseISO } from "date-fns"
+
+interface TopLoggerTimeSlot {
+  id: number
+  reservation_area_id: number
+  start_at: string
+  end_at: string
+  checkin_end_at: string
+  spots: number
+  spots_booked: number
+  details: string | null
+  require_client_number: boolean
+  live: boolean
+  require_password: boolean
+}
+
+interface AvailableTimeSlot {
+  id: number
+  details: string | null
+  numberOfSpots: number
+  startSlot: string
+}
+
+const checkSlot = (slot: TopLoggerTimeSlot) => {
+  const { spots_booked, spots, start_at, details, id } = slot
+
+  if (spots_booked < spots) {
+    return {
+      id,
+      details,
+      numberOfSpots: spots - spots_booked,
+      startSlot: start_at,
+    }
+  } else return null
+}
+
+const getSlotsWithin75MinutesFromNow = (
+  slots: TopLoggerTimeSlot[],
+  now: Date
+) => {
+  return slots.filter((slot) => {
+    const diff = differenceInMinutes(parseISO(slot.start_at), now)
+    return diff <= 75 && !(diff < 0)
+  })
+}
+
+export const extractAvailableSlots = (
+  slots: TopLoggerTimeSlot[],
+  now: Date
+) => {
+  const availableSlots: AvailableTimeSlot[] = []
+
+  const slotsInRange = getSlotsWithin75MinutesFromNow(slots, now)
+  slotsInRange.forEach((slot) => {
+    const available = checkSlot(slot)
+    if (available) availableSlots.push(available)
+    else return
+  })
+
+  return availableSlots.length ? availableSlots : null
+}
